5c8f00f7e99990f8b49271326d9272a1a438667b
krishbhardwaj075-source <your_github_email@example.com>
2025-11-10 23:04:15 +0530

    new changes

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def index():
 # ---------- MAIN ENTRY ----------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render assigns dynamic port
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
